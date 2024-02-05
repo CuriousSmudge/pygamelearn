297ffdcc9181b749a98ea91c7ad3d37387684514
Knaughty <Dawn.lane@thedropbears.org.au>
2024-02-06 10:46:38 +1100

    Baseline template i think

--- car.py
+++ car.py
@@ -2,10 +2,20 @@ N = 3
 systemSize = 1
 Cycles = 3
 
+RedArr = [0, 1, 1, 1, 0 ,0, 1]
+BlueArr = [1, 0, 1, 0, 0 ,0, 1]
+
+def traffic (N, systemSize, Cycles, RedArr, BlueArr):
+    for i in range(Cycles):
+        RedArr = redCycle(RedArr, BlueArr)
+        BlueArr = blueCycle(RedArr, BlueArr)
 
 
+def redCycle (RedArr, BlueArr):
+    return RedArr
 
-def traffic (N, systemSize, Cycles):
 
+def blueCycle (RedArr, BlueArr):
+    return BlueArr
 
-    pass
+traffic()
