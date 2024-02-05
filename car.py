165d5ea90702b37136169a089f3e7d16facdd05a
Knaughty <Dawn.lane@thedropbears.org.au>
2024-02-06 10:50:27 +1100

    Comments and stuff

--- car.py
+++ car.py
@@ -1,21 +1,20 @@
+# Variables
 N = 3
 systemSize = 1
 Cycles = 3
-
 RedArr = [0, 1, 1, 1, 0 ,0, 1]
 BlueArr = [1, 0, 1, 0, 0 ,0, 1]
 
+# Cycle Loop
 def traffic (N, systemSize, Cycles, RedArr, BlueArr):
     for i in range(Cycles):
-        RedArr = redCycle(RedArr, BlueArr)
-        BlueArr = blueCycle(RedArr, BlueArr)
+        RedArr = Cycle(RedArr, BlueArr)
+        BlueArr = Cycle(BlueArr, RedArr)
+
+# Doing (ur mom doin doin ur mom) Simulation
+def Cycle (Arr1, Arr2):
+    return Arr1
 
 
-def redCycle (RedArr, BlueArr):
-    return RedArr
-
-
-def blueCycle (RedArr, BlueArr):
-    return BlueArr
 
 traffic()
