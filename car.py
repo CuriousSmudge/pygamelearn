123f87cf5407b650a4e6ea4dab93f85163a10b1a
Knaughty <Dawn.lane@thedropbears.org.au>
2024-02-06 11:13:28 +1100

    Update car.py

--- car.py
+++ car.py
@@ -8,13 +8,23 @@ BlueArr = ["B", ".", "B", ".", ".", ".", "B"]
 # Cycle Loop
 def traffic (N, systemSize, Cycles, RedArr, BlueArr):
     for i in range(Cycles):
-        RedArr = Cycle(RedArr, BlueArr)
-        BlueArr = Cycle(BlueArr, RedArr)
+        RedArr = Cycle(RedArr, BlueArr, "R")
+        BlueArr = Cycle(BlueArr, RedArr, "B")
 
 # Doing (ur mom doin doin ur mom) Simulation
-def Cycle (Arr1, Arr2):
+def Cycle (Arr1, Arr2, Type):
+
+    for x in Arr1:
+        if x == N: # just before intersection
+            if Arr2[x + 1] != ".": # Check if empty
+
+
+        elif Arr1[x + 1] == Type:
+            Arr1[x] == "."
+            Arr1[x + 1] == Type
     return Arr1
 
 
 
-traffic()
+
+print(Cycle(RedArr, BlueArr, "R"))
