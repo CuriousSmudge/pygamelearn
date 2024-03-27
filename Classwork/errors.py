c095a3035b3a7b08c2f731f217c89bf52bb229ff
CuriousSmudge <120012535+CuriousSmudge@users.noreply.github.com>
2024-03-27 14:05:28 +1100

    error classwork

---
+++ Classwork/errors.py
@@ -0,0 +1,8 @@
+myVars = [1, 2, 3, 4]
+counter = 0
+steps = 2
+total = 0
+while counter < len(myVars) : 
+    total += myVars[counter]
+    counter += 1
+print(f"The total of the numbers is : {total}.")
